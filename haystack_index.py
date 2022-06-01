b3ec7eb3f64a47efd3de6d5ec259c770b8486192
Stella <aispacevir4812@gmail.com>
2022-06-01 12:07:00 +0300

    Indices-Range-Break-From-A-Loop

--- haystack_index.py
+++ haystack_index.py
@@ -7,26 +7,31 @@ haystack = "mississipi"
 needle = 'issip'
 
 def indices_range(hay, nee):
-    if hay == None or nee not in hay:
-        return -1
-    else:
-        for i in range(len(hay) - len(nee) + 1):
-            if nee == hay[i: i + len(nee)]:
-                return i
+    if nee == '':
+        return 0
+
+    for i in range(len(hay) - len(nee) + 1):
+        if nee == hay[i: i + len(nee)]:
+            return i
+     return -1
 
 print(indices_range(haystack,  needle))
 
 def index(hay, nee):
     if hay == None or nee not in hay:
-        return -1
-    else:
-        for i in range(len(hay)):
-            for j in range(len(nee)):
-                print(i, j, hay[i], nee[j])
-                if nee[j] == hay[i]:
-                    return i
+        return 0
 
-# print(index(haystack,  needle))
+    for i in range(len(hay)):
+        for j in range(len(nee)):
+            if hay[i + j] != nee[j]:
+                print('Not equal')
+                break
+            if j == len(nee) - 1:
+                print('Equal')
+                return i
+    return -1
+
+print(index(haystack,  needle))
 
 
 
